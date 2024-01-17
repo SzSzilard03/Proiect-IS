17dcdef65aef1b5b3582f6d4371c2b2dfb0658b6
TDragosM <tartagandragos@gmail.com>
2024-01-17 15:12:50 +0200

    SignUp v3

--- app/src/main/java/com/myapplication/SignUpActivity.kt
+++ app/src/main/java/com/myapplication/SignUpActivity.kt
@@ -1,22 +1,49 @@
 package com.myapplication
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.ComponentActivity
-import androidx.appcompat.app.AppCompatActivity
+
 class SignUpActivity :
-    AppCompatActivity() {
+    ComponentActivity() {
 
-        override fun onCreate(savedInstanceState: Bundle?) {
-            super.onCreate(savedInstanceState)
-            setContentView(R.layout.sign_up)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.sign_up)
 
-            // Now you can continue with your logic, such as initializing views, handling user input, etc.
-            val signUpInstance = SignUp("exampleUser", "password123", "user@example.com")
-            val isSignUpSuccessful = signUpInstance.signUpUser()
+        // Now you can continue with your logic, such as initializing views, handling user input, etc.
+        val signUpInstance = SignUp("exampleUser", "password123", "user@example.com")
+        val isSignUpSuccessful = signUpInstance.signUpUser()
+        val backButton1 = findViewById<Button>(R.id.buttonBack1)
+        val buttonSignUpAttempt = findViewById<Button>(R.id.buttonSignUpAttempt)
+        backButton1.setOnClickListener {
+            // Create an Intent to start the main activity
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
 
-            if (isSignUpSuccessful) {
-                println("Welcome! Your account has been created.")
-            } else {
-                println("Registration failed. Please try again.")
-            }
+            // Finish the current activity (SignUpActivity)
+            finish()
         }
+
+
+        if (isSignUpSuccessful) {
+            println("Welcome! Your account has been created.")
+
+        } else {
+            println("Registration failed. Please try again.")
+        }
+        try {
+            buttonSignUpAttempt.setOnClickListener {
+                // Create an Intent to start the main activity
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+
+                // Finish the current activity (SignUpActivity)
+                finish()
+            }
+        } catch (e: Exception) {
+            e.printStackTrace()
+            // Log the exception or handle it appropriately
+        }
+    }
 }
