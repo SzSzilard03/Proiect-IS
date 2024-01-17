a75dae1432a859ac09d3e05d959b2e4960e9975d
navem-laith <abdallahlaith@yahoo.com>
2024-01-17 18:34:22 +0200

    database good? try1

--- app/src/main/java/com/myapplication/DataBaseHandler.kt
+++ app/src/main/java/com/myapplication/DataBaseHandler.kt
@@ -1,6 +1,8 @@
 package com.myapplication
 
+import android.content.ContentValues
 import android.content.Context
+import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
 
 val DATABASE_NAME = "my_database"
@@ -8,3 +10,24 @@ val TABLE_NAME = "Players"
 val COL_NAME = "username"
 val COL_AGE = "age"
 val COL_ID = "id"
+class DataBaseHandler (context : Context) : SQLiteOpenHelper(context, DATABASE_NAME, null, 1){
+    override fun onCreate(p0: SQLiteDatabase?) {
+        val createTable =
+            "CREATE TABLE $TABLE_NAME ($COL_ID INTEGER PRIMARY KEY AUTOINCREMENT, $COL_NAME VARCHAR(256), $COL_AGE INTEGER)"
+        p0?.execSQL(createTable)
+    }
+
+    override fun onUpgrade(p0: SQLiteDatabase?, p1: Int, p2: Int) {
+        TODO("Not yet implemented")
+    }
+    fun insertData(player: Player){
+        val db = this.writableDatabase
+        var cv = ContentValues()
+        cv.put(COL_NAME, player.name)
+        cv.put(COL_AGE, player.age)
+        var res = db.insert(TABLE_NAME, null, cv)
+        if(res.toInt() == -1)
+            println("couldnt load db!")
+        else println("successfully load db!")
+    }
+}
---
+++ app/src/main/java/com/myapplication/Player.kt
@@ -0,0 +1,12 @@
+package com.myapplication
+
+class Player {
+    var playerId : Int = 0
+    var age : Int = 18
+    var name : String = ""
+
+    constructor(name:String, age:Int){
+        this.name = name
+        this.age = age
+    }
+}
