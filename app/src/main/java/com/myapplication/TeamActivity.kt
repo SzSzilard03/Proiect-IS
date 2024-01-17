3778c7d1b30e23a202ac33a7e63ee2674e202625
TDragosM <tartagandragos@gmail.com>
2024-01-18 00:26:39 +0200

    Pagini noi

--- app/src/main/java/com/myapplication/TeamActivity.kt
+++ app/src/main/java/com/myapplication/TeamActivity.kt
@@ -1,4 +1,14 @@
 package com.myapplication
 
-class TeamActivity {
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.ComponentActivity
+import androidx.cardview.widget.CardView
+class TeamActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.team_page)
+    }
 }
