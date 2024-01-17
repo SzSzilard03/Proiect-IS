38439cbdb4c37412aa285c9bf182e26f5940f495
Szili <sziliszoll54@gmail.com>
2024-01-17 18:48:28 +0200

    na ni ma ce am facut

--- app/src/main/java/com/myapplication/SignUpActivity.kt
+++ app/src/main/java/com/myapplication/SignUpActivity.kt
@@ -42,7 +42,7 @@ class SignUpActivity : ComponentActivity() {
             val isSignUpSuccessful = signUpInstance.signUpUser()
 
             if (isSignUpSuccessful) {
-                var db = DataBaseHandler(context)
+                var db = DataBaseHandler(context = this)
                 val temp = Player(fullName, 20)
                 db.insertData(temp)
                 showToast("Welcome! Your account has been created.")
