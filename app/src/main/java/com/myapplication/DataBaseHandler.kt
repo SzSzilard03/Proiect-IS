c7c19a822d0b03a5747951b216222b4b9fbafc21
navem-laith <abdallahlaith@yahoo.com>
2024-01-17 19:53:46 +0200

    database players table almost done

--- app/src/main/java/com/myapplication/DataBaseHandler.kt
+++ app/src/main/java/com/myapplication/DataBaseHandler.kt
@@ -1,20 +1,38 @@
 package com.myapplication
 
+import android.annotation.SuppressLint
 import android.content.ContentValues
 import android.content.Context
 import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
 
 val DATABASE_NAME = "my_database"
-val TABLE_NAME = "Players"
-val COL_NAME = "username"
-val COL_AGE = "age"
-val COL_ID = "id"
+
+//TABLES
+val PLAYERS_TABLE = "players"
+
+//COLUMNS
+val PLAYER_ID = "id"
+val PLAYER_NAME = "name"
+val PLAYER_AGE = "age"
+val PLAYER_USERNAME = "username"
+val PLAYER_EMAIL = "email"
+val PLAYER_PASSWORD = "password"
+val PLAYER_POSITION = "position"
+
 class DataBaseHandler (context : Context) : SQLiteOpenHelper(context, DATABASE_NAME, null, 1){
+
     override fun onCreate(p0: SQLiteDatabase?) {
-        val createTable =
-            "CREATE TABLE $TABLE_NAME ($COL_ID INTEGER PRIMARY KEY AUTOINCREMENT, $COL_NAME VARCHAR(256), $COL_AGE INTEGER)"
-        p0?.execSQL(createTable)
+        val createPlayersTable =
+            "CREATE TABLE $PLAYERS_TABLE (" +
+                    "$PLAYER_ID INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                    "$PLAYER_NAME VARCHAR(50), " +
+                    "$PLAYER_AGE INTEGER, " +
+                    "$PLAYER_USERNAME VARCHAR(50), " +
+                    "$PLAYER_EMAIL VARCHAR(50), " +
+                    "$PLAYER_PASSWORD VARCHAR(50), " +
+                    "$PLAYER_POSITION VARCHAR(50))"
+        p0?.execSQL(createPlayersTable)
     }
 
     override fun onUpgrade(p0: SQLiteDatabase?, p1: Int, p2: Int) {
@@ -23,11 +41,37 @@ class DataBaseHandler (context : Context) : SQLiteOpenHelper(context, DATABASE_N
     fun insertData(player: Player){
         val db = this.writableDatabase
         var cv = ContentValues()
-        cv.put(COL_NAME, player.name)
-        cv.put(COL_AGE, player.age)
-        var res = db.insert(TABLE_NAME, null, cv)
+        cv.put(PLAYER_NAME, player.name)
+        cv.put(PLAYER_AGE, player.age)
+        cv.put(PLAYER_USERNAME, player.username)
+        cv.put(PLAYER_EMAIL, player.email)
+        cv.put(PLAYER_PASSWORD, player.password)
+        cv.put(PLAYER_POSITION, player.position)
+        var res = db.insert(PLAYERS_TABLE, null, cv)//id este autoincrementat
         if(res.toInt() == -1)
             println("couldnt load db!")
         else println("successfully load db!")
+    }
+    @SuppressLint("Range")
+    fun getAllPlayerNames(): List<String> {
+        val namesList = mutableListOf<String>()
+        val db = this.readableDatabase
+
+        // Specify the columns you want to retrieve
+        val columns = arrayOf(PLAYER_NAME)
+
+        // Query the database to get all names
+        val cursor = db.query(PLAYERS_TABLE, columns, null, null, null, null, null)
+
+        // Iterate through the cursor and add names to the list
+        cursor?.use {
+            while (it.moveToNext()) {
+                val playerName = it.getString(it.getColumnIndex(PLAYER_NAME))
+                namesList.add(playerName)
+            }
+        }
+        cursor?.close()
+        db.close()
+        return namesList
     }
 }
