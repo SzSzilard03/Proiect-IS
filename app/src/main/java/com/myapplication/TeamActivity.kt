14558dd74d560746fd5e795c2ccfbb518750f4bb
TDragosM <tartagandragos@gmail.com>
2024-01-18 00:42:18 +0200

    TeamACtivity v1

--- app/src/main/java/com/myapplication/TeamActivity.kt
+++ app/src/main/java/com/myapplication/TeamActivity.kt
@@ -10,6 +10,42 @@ class TeamActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.team_front)
+        val backBtn = findViewById<CardView>(R.id.back_btn)
+        backBtn.setOnClickListener {
+            //showToast("ahoy")
+            val intent = Intent(this, MainPageActivity::class.java)// Return
+            startActivity(intent)
+            finish()
+        }
+        val resultsBtn = findViewById<CardView>(R.id.results_btn)
+        resultsBtn.setOnClickListener {
+            //showToast("ahoy")
+            val intent = Intent(this, MainPageActivity::class.java)// ResultsActivity
+            startActivity(intent)
+            finish()
+        }
+        val chatBtn = findViewById<CardView>(R.id.chat_btn)
+        chatBtn.setOnClickListener {
+            //showToast("ahoy")
+            val intent = Intent(this, MainPageActivity::class.java)// ChatActivity
+            startActivity(intent)
+            finish()
+        }
+        val fixturesBtn = findViewById<CardView>(R.id.fixtures_btn)
+        fixturesBtn.setOnClickListener {
+            //showToast("ahoy")
+            val intent = Intent(this, MainPageActivity::class.java)// MembersActivity
+            startActivity(intent)
+            finish()
+        }
+        val membersBtn = findViewById<CardView>(R.id.members_btn)
+        membersBtn.setOnClickListener {
+            //showToast("ahoy")
+            val intent = Intent(this, MainPageActivity::class.java)// FixturesActivity
+            startActivity(intent)
+            finish()
+        }
+
     }
     private fun showToast(message: String) {
         Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
