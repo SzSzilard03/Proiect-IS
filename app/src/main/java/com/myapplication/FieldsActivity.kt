9a514a54eb7aef3f4137c68f84a537fee8c797cf
TDragosM <tartagandragos@gmail.com>
2024-01-18 02:32:27 +0200

    More changes

--- app/src/main/java/com/myapplication/FieldsActivity.kt
+++ app/src/main/java/com/myapplication/FieldsActivity.kt
@@ -2,6 +2,7 @@ package com.myapplication
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.cardview.widget.CardView
@@ -11,12 +12,22 @@ class FieldsActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.fields_front)
         val backBtn = findViewById<CardView>(R.id.back_btn)
+        val textSet = findViewById<TextView>(R.id.textViewData)
         backBtn.setOnClickListener {
             //showToast("ahoy")
             val intent = Intent(this, MainPageActivity::class.java)// Return
             startActivity(intent)
             finish()
         }
+
+        var db = DataBaseHandler(context = this)
+        val temp = db.getAllFieldNames()
+        if(temp.isEmpty())
+        {
+            db.populateFieldTable()
+        }
+        val concatenatedString = temp.joinToString(separator = "\n")
+        textSet.text = concatenatedString
     }
     private fun showToast(message: String) {
         Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
