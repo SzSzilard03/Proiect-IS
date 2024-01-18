54f5d9d4ad0e7e3d15b192aa4e7c103f6a89dc52
navem-laith <abdallahlaith@yahoo.com>
2024-01-18 04:33:15 +0200

    calendar1

--- app/src/main/java/com/myapplication/model/Calendar.kt
+++ app/src/main/java/com/myapplication/model/Calendar.kt
@@ -1,5 +1,15 @@
 package com.myapplication.model
 
 class Calendar {
-    var calendarId : Int = 0
+    var calendarId: Int = 0
+    var userId: Int = 0
+    var fixtures: MutableList<Fixture> = mutableListOf()
+
+    inner class Date(val day: Int, val month: Int, val year: Int)
+
+    inner class Fixture(
+        val team1: Team = Team(0, "manu", 0),
+        val team2: Team = Team(0, "manc", 1),
+        val date: Date = Date(1, 1, 2023)
+    )
 }
